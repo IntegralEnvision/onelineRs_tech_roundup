3153d9e8449faebfa22ccd2fcfe9f51186426d95
Jonathan Zadra <jzadra@integral-corp.com>
2023-06-05 13:56:59 -0600

    add several new one liners

---
+++ oneliners/_ideas.R
@@ -0,0 +1,8 @@
+# Ideas for future
+From Katherine:
+Easy one-liner: https://eliocamp.github.io/ggnewscale/ . 
+I recently used it to have two fill options on this ggplot!
+
+  
+  ----
+.Last.value

279265c61872803422354d1579c97e51b0a22a12
Jonathan Zadra <jzadra@integral-corp.com>
2023-09-25 11:32:38 -0700

    update with new molr

--- oneliners/_ideas.R
+++
@@ -1,8 +0,0 @@
-# Ideas for future
-From Katherine:
-Easy one-liner: https://eliocamp.github.io/ggnewscale/ . 
-I recently used it to have two fill options on this ggplot!
-
-  
-  ----
-.Last.value
